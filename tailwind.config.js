b18f5fd6fee0ad3c50b0e54e80ebc9aeae1f3150
SKrishna-7 <sureshkrishna375@gmail.com>
2025-04-01 21:26:08 +0530

    sm update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,10 @@
 
 module.exports = {
   content:[
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}", // Add this if using App Router
+    "./*.{js,jsx,ts,tsx}", // Include root-level files if needed
   ],
   theme: {
     extend: {
